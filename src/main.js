8f9845e9103ef4f39850d80beef232658f06a805
Hongxu Liu <djshu.us@gmail.com>
2014-10-20 18:50:27 -0700

    Added FastClick for mobile

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ define(function(require, exports, module) {
     var Engine = require('famous/core/Engine');
     var AppView = require('views/AppView');
     var AppConfig = require('data/AppConfig');
+    require('famous/inputs/FastClick');
 
     var mainContext = Engine.createContext();
     var appView = new AppView(AppConfig);
